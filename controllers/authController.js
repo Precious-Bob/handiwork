b62e47e1a2e2f3f83fed8add5e5947312237de13
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-02-21 15:59:56 +0100

    login validation fixes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,18 @@
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res) => {
   try {
-    const { firstName, lastName, email, address, password, confirmPassword } =
-      req.body;
-    if (password !== confirmPassword)
-      return res.status(400).json({ message: 'Passwords do not match' });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    req.body.password = hashedPassword;
+    // const { firstName, lastName, email, address, password, confirmPassword } =
+    //   req.body;
+    // if (password !== confirmPassword)
+    //   return res.status(400).json({ message: 'Passwords do not match' });
 
     const newUser = await User.create(req.body);
+    const token = newUser.generateAuthToken();
 
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUser,
       },
--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,92 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
 
+// Define Joi schema
+const userJoiSchema = Joi.object({
+  firstName: Joi.string().alphanum().min(3).max(30).required().messages({
+    'any.required': 'Please provide your first name',
+  }),
+  lastName: Joi.string().required().min(3).max(30).messages({
+    'any.required': 'Please provide your last name',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.email': 'Please provide a valid email',
+    'any.required': 'Please provide your email',
+  }),
+  address: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+    'any.only': 'Passwords do not match',
+  }),
+});
+
+// Define Mongoose schema
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: [true, 'Please provide your first name'],
+    required: true,
   },
   lastName: {
     type: String,
-    required: [true, 'Please provide your last name'],
+    required: true,
   },
   email: {
     type: String,
-    required: [true, 'Please provide your email'],
-    unique: [true, 'This email already exists'],
+    required: true,
+    unique: true,
     lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email'],
   },
-  address: { type: String },
-  password: { type: String },
+  address: String,
+  password: String,
+  confirmPassword: String,
 });
 
+userSchema.pre('save', async function (next) {
+  try {
+    // Make a copy of the user data
+    const userData = { ...this.toObject() };
+
+    // Delete the _id field
+    delete userData._id;
+
+    // Validate the user data without _id field
+    await userJoiSchema.validateAsync(userData);
+
+    // Check if the password is modified and hash it if necessary
+    if (this.isModified('password')) {
+      const salt = await bcrypt.genSalt(12);
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+
+    // Proceed to the next middleware
+    next();
+  } catch (error) {
+    // Pass any validation errors to the next middleware
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+//  take note of this in adding admin if needed { _id: user._id, admin: user.admin },
+userSchema.methods.generateAuthToken = function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+  return token;
+};
+
+//delete confirmPassword
+userSchema.methods.toJSON = function () {
+  const userObj = this.toObject();
+  delete userObj.confirmPassword;
+  return userObj;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
