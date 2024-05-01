a55af291dad0e1aedc92200d514f8311f2539b33
Pr3ciousXX <preciousbobmanuel501@gmail.com>
2024-05-01 18:38:50 +0100

    added styling to docs

--- app.js
+++ app.js
@@ -82,11 +82,38 @@ app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/docs', swaggerRoute);
 
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send(
-      `Welcome to Handiwork API. click on <a href="https://handiwork-api.onrender.com/api/v1/docs/">/docs</a> to see the documentation and test the endpoints.`
-    );
+  res.status(200).send(
+    `<html>
+      <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #333; /* dark gray background */
+          }
+          .message {
+            color: #fff; /* white text color */
+            font-size: 36px;
+            font-weight: bold;
+          }
+          .message a { /* added this to target the link */
+            color: #0074D9; /* blue link color */
+            text-decoration: none;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="message">
+          🚀Welcome to the Handiwork API!<br>
+          Please visit <a href="/docs">/docs</a> to see the documentation📚<br> and test the endpoints.
+        </div>
+      </body>
+    </html>`
+  );
 });
 
 app.all('*', (req, res, next) => {
